5648738fddd0ac2d61304e911431db3e9077d91a
Matt Goldhaber <mgoldhab@umich.edu>
2014-03-18 13:06:30 -0400

    My Initial commit. Spawners, enemies, camera, player

---
+++ Assets/Scripts/AIScript/ExampleEnemy/SampleAIController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAIController : MonoBehaviour {
+
+	//player to move towards
+	public Transform player;
+
+	public float speed = 1.0f;
+
+	public int health = 3;
+
+	void Start()
+	{
+		if( player == null )
+		{
+			print ("WTF");
+		}
+		player = PlayerController.playerSingleton;
+	}
+
+	void Update () {
+
+		this.transform.LookAt(player);
+
+		transform.position = Vector3.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+
+		//check health
+
+		if( health <= 0 )
+		{
+			Remove();
+		}
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if( coll.gameObject.tag == "Player" )
+		{
+			Remove();
+		}
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if( coll.gameObject.tag == "PlayerBullet" )
+		{
+			health--;
+		}
+	}
+
+	public void Remove()
+	{
+		this.gameObject.GetComponent<MGAISuperClass>().Remove();
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float movementSpeed = 10;
+	public float strafeSpeed = 8;
+
+	public float fireRate = .11f;
+	private float lastShot = -10;
+
+	public float fireSpeed = 4;
+	public GameObject fireProj;
+
+	public Transform thisCamera;
+
+	public static Transform playerSingleton;
+	// Use this for initialization
+	void Start () {
+	
+		playerSingleton = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//Movement
+		float horizontal = Input.GetAxis("Horizontal") * strafeSpeed * Time.deltaTime;
+		transform.Translate(horizontal, 0, 0);
+		
+		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate(0, 0, vertical);
+
+		//Ability
+		if(Input.GetMouseButton(0)){
+			if(Time.time > fireRate+lastShot){
+				GameObject clone = (GameObject) Instantiate(fireProj, gameObject.transform.position + (new Vector3(0,gameObject.transform.renderer.bounds.size.y/2 ,0)) + (gameObject.transform.forward.normalized * 1), gameObject.transform.rotation) as GameObject;
+				//fireProj.tag = "Bullet";
+				clone.rigidbody.velocity = thisCamera.transform.forward * fireSpeed * Time.deltaTime;
+				
+				lastShot = Time.time;
+			}
+		}
+	}
+}
