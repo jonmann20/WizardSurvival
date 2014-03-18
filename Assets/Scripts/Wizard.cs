d6b83b5f32f1fc4bcfbaba25d03a5a204167adb1
Jon Wiedmann <jonwiedmann@gmail.com>
2014-03-18 10:31:23 -0400

    git init

---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.W)) {
+            rigidbody.velocity += new Vector3(0, 0, 15 * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S)) {
+            rigidbody.velocity += new Vector3(0, 0, -15 * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            rigidbody.velocity += new Vector3(-15 * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.D)) {
+            rigidbody.velocity += new Vector3(15 * Time.deltaTime, 0, 0);
+        }
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,28 @@ WizardSurvival
 ==============
 
 A wave survival game in Unity.
+
+Characters Ideas
+----------------
+* Ryu (Street Fighter)
+	* fireballs (hadouken)
+* Jafar (Alladin)
+	* enchantment
+	* minions
+* Tim (Monty Python and the Holy Grail)
+	* levitation
+		* wind
+		* fus ro dah??
+	* comedy lines
+* Gandolf (LOTR)
+	* melee
+	* forcefield (defense)
+	
+Enemy Ideas
+-----------
+* ghosts
+* multi headed dragons
+* garden gnomes
+* knights
+	
+	
