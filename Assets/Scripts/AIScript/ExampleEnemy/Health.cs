e4109022b7e60177c5fc4f012623e833184503d5
Matt Goldhaber <mgoldhab@umich.edu>
2014-03-26 12:48:57 -0400

    +Skeleton Death Anim 	-hooked up health for them. should take 4 shots

---
+++ Assets/Scripts/AIScript/ExampleEnemy/Health.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class Health : MonoBehaviour {
+	public float health = 100f;
+	public AIRig aiRig = null;
+
+	// Use this for initialization
+	void Start () {
+		aiRig = gameObject.GetComponentInChildren<AIRig>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		aiRig.AI.WorkingMemory.SetItem("health", health);
+	}
+
+	public void Damage( float damage )
+	{
+		health = Mathf.Clamp(health-damage,0,health);
+	}
+}
