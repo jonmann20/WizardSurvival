fd75988273d683288f7efeb70cd4c0f16d58d2af
Jon Wiedmann <jonwiedmann@gmail.com>
2014-04-07 12:12:28 -0400

    fix tome

--- Assets/Scripts/Tome.cs
+++ Assets/Scripts/Tome.cs
@@ -11,37 +11,40 @@ public class Tome : MonoBehaviour {
 	public GameObject dialog;
 	public BaseAbility baseAbility;
 
-	void Update(){
-		InputDevice device = InputManager.ActiveDevice;
-		InputControl ctrl_Action = device.GetControl(InputControlType.Action3);
+	bool dialogVisible = false;
 
-		if(ctrl_Action.WasPressed){
-			if(AbilityManagerScript.currentAbility){
-				// alert!! can't pickup more than one base ability
-			}
-			else {
-				AbilityManagerScript.that.changeAbility(Enum.GetName(typeof(BaseAbility), baseAbility));
-				Destroy(gameObject);
+	void Update(){
+		if(dialogVisible){
+			// dialog
+			dialog.transform.rotation = Quaternion.LookRotation(GameObject.Find("MainCamera").transform.forward);
+
+			InputDevice device = InputManager.ActiveDevice;
+			InputControl ctrl_Action = device.GetControl(InputControlType.Action3);
+
+			if(ctrl_Action.WasPressed){
+				if(AbilityManagerScript.currentAbility){
+					// alert!! can't pickup more than one base ability
+					GameAudio.playInvNoMove();
+				}
+				else {
+					AbilityManagerScript.that.changeAbility(Enum.GetName(typeof(BaseAbility), baseAbility));
+					Destroy(gameObject);
+				}
 			}
 		}
 	}
 
 	void OnTriggerEnter(Collider col){
-		showDialog(col);
-	}
-
-	void OnTriggerStay(Collider col){
-		showDialog(col);
-	}
-
-	void OnTriggerExit(){
-		dialog.SetActive(false);
-	}
-
-	void showDialog(Collider col){
 		if(col.gameObject.tag == "Player"){
+			dialogVisible = true;
 			dialog.SetActive(true);
-			dialog.transform.rotation = Quaternion.LookRotation(GameObject.Find("MainCamera").transform.forward);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Player"){
+			dialogVisible = false;
+			dialog.SetActive(false);
 		}
 	}
 }
