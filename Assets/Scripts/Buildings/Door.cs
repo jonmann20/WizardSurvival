0fcbbcb245b0c23ba992a9683419a560fbb209ba
Eugene Goh <vanblaze5@gmail.com>
2014-04-14 14:17:36 -0400

    Added door script

--- Assets/Scripts/Buildings/Door.cs
+++ Assets/Scripts/Buildings/Door.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
+	private int door_LastIndex;
+	private bool doorOpen = false;
+	private int count = 0;
 	// Use this for initialization
-	void Start () {
-	
+	void playDoorAnim () {
+		if (door_LastIndex == 0) {
+			animation.Play("DoorLeftOpen");
+			animation.Play("DoorRightOpen");
+		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (doorOpen == false) {
+			count = 0;
+			for( int i = 0; i < PhotonNetwork.playerList.Length; i++ ) {
+				if( PhotonNetwork.playerList[i].customProperties.ContainsKey("Ability") ) {
+					++count;
+				}
+			}
+			if (count == PhotonNetwork.playerList.Length - 1) {
+				doorOpen = true;
+				animation.Play("DoorLeftOpen");
+				animation.Play("DoorRightOpen");
+				count = 0;
+			}
+		}
 	}
 }
