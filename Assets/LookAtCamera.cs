0081fdd5e26101954e4c8cc79a3c1edd3ac3f023
ayarger <ayarger@umich.edu>
2014-04-19 03:12:40 -0400

    Made critical changes to Jumpy Wizard and King Wizard

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	GameObject MainCamera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(MainCamera == null)
+			MainCamera = GameObject.FindWithTag("MainCamera");
+		else
+			transform.rotation = Quaternion.LookRotation(MainCamera.transform.forward);
+	}
+}
