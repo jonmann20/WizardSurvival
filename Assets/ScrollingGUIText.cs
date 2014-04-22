b7384f033063496ac7ffed7a82526ffeb4a7a7ce
Eugene Goh <vanblaze5@gmail.com>
2014-04-22 12:56:56 -0400

    Added intro music to credits, credits done

--- Assets/ScrollingGUIText.cs
+++ Assets/ScrollingGUIText.cs
@@ -7,15 +7,16 @@ public class ScrollingGUIText : MonoBehaviour {
 	bool scrolling = false;
 
 	void Start () {
+		GameAudio.playIntro ();
 		scrolling = true;
 		GUIText credits = (GUIText) this.GetComponent (typeof(GUIText));
-		string creds = "Defend Thy Kingdom\n\n\n";
+		string creds = "Defend Thy Kingdom\n\n\n\n\n";
 		creds += "Production Team:\n\n";
 		creds += "Jon Wiedmann\n";
 		creds += "Austin Yarger\n";
 		creds += "Matthew Goldhaber\n";
 		creds += "Eugene Goh\n\n\n";
-		creds += "Many thanks to EECS494 students for your awesome feedback and playtests\n\n";
+		creds += "Many thanks to EECS494 for your awesome feedback and playtests\n\n";
 		creds += "Many more thanks to our awesome Jeremy Gibson and Isaiah Hines\n\n";
 		credits.text = creds;
 	}
@@ -26,7 +27,7 @@ public class ScrollingGUIText : MonoBehaviour {
 		} else {
 		transform.Translate (Vector3.up * Time.deltaTime * speed);
 		}
-		if (gameObject.transform.position.y > -0.015f) {
+		if (gameObject.transform.position.y > -0.030f) {
 			scrolling = false;
 		}
 	}
