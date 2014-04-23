c6c1aed4f2faa7d1e4cc07cc2d51ef57b1ae657c
Eugene Goh <vanblaze5@gmail.com>
2014-04-23 14:37:10 -0400

    Removed wood package (unused), edited doorleft and doorright to force four player requirement, edited assetlist

--- Assets/Scripts/Buildings/DoorLeft.cs
+++ Assets/Scripts/Buildings/DoorLeft.cs
@@ -9,7 +9,7 @@ public class DoorLeft : MonoBehaviour {
 	
 	void Update () {
 		if (door_LastIndex == 0) {
-			count = PhotonNetwork.playerList.Length;
+			count = 4;
 			for (int i = 0; i < PhotonNetwork.playerList.Length; ++i) {
 				string p = (string) PhotonNetwork.playerList[i].customProperties["Ability"];
 				if (p != null && p != "none") {
