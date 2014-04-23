1e3e85c6e5337dbd1d44ab478d2b0ee118eaa835
ayarger <ayarger@umich.edu>
2014-04-23 15:00:23 -0400

    Disabled networking of speed-- Was currently throwing errors. Will need to revise for playtest version.

--- Assets/Scripts/AI/Enemy/SampleAIController.cs
+++ Assets/Scripts/AI/Enemy/SampleAIController.cs
@@ -76,7 +76,7 @@ public class SampleAIController : MonoBehaviour {
 		//speed
 		float[] speedParam = new float[1];
 		speedParam[0] = speed;
-		view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
+		//view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
 
 
 		//health = transform.parent.transform.GetComponent<Health>().health;
@@ -105,7 +105,7 @@ public class SampleAIController : MonoBehaviour {
 				float[] speedParam = new float[1];
 				speedParam[0] = speed * speedReduction;
 				PhotonView view = PhotonView.Find(this.transform.parent.GetComponent<PhotonView>().viewID);
-				view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
+				//view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
 				speedReduced = false;
 			}
 
@@ -117,7 +117,7 @@ public class SampleAIController : MonoBehaviour {
 			float[] speedParam = new float[1];
 			speedParam[0] = speed;
 			PhotonView view = PhotonView.Find(this.transform.parent.GetComponent<PhotonView>().viewID);
-			view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
+			//view.RPC("setSpeedRPC",PhotonTargets.All, speedParam);
 			speedReduced = false;
 		}
 	}
