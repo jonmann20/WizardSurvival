d54af6cff487aa4367824e21d0b0e28e1e5f1f13
Eugene Goh <vanblaze5@gmail.com>
2014-04-23 15:00:07 -0400

    removed four person requirement

--- Assets/Scripts/Buildings/DoorRight.cs
+++ Assets/Scripts/Buildings/DoorRight.cs
@@ -9,7 +9,7 @@ public class DoorRight : MonoBehaviour {
 
 	void Update () {
 		if (door_LastIndex == 0) {
-				count = 4;
+				count = PhotonNetwork.playerList.Length;
 				for (int i = 0; i < PhotonNetwork.playerList.Length; ++i) {
 						string p = (string)PhotonNetwork.playerList [i].customProperties ["Ability"];
 						if (p != null && p != "none") {
